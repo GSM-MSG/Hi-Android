6be856ee21164312a914e900447f36f8a0281a12
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-02-27 14:17:49 +0900

    비밀번호가 다를 시 토스트메시지 출력 완성

--- app/src/main/java/com/example/hiprojecttest/view/signup/fragment/MakingPassFragment.kt
+++ app/src/main/java/com/example/hiprojecttest/view/signup/fragment/MakingPassFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -36,12 +37,11 @@ class MakingPassFragment : Fragment() {
 
         var newPass = binding.passwordInputBox.text.toString()
         var checkingPass = binding.passInputBox.text.toString()
-
-        if (newPass == checkingPass) {
-            binding.passInputBox.setOnTextChanged { p0, p1, p2, p3 ->
-                if (!p0.isNullOrBlank()) {
-                    binding.nextStepBtn.setBackground(resources.getDrawable(R.drawable.gradient_btn))
-                    binding.nextStepBtn.setOnClickListener {
+        binding.passInputBox.setOnTextChanged { p0, p1, p2, p3 ->
+            if (!p0.isNullOrBlank()) {
+                binding.nextStepBtn.setBackground(resources.getDrawable(R.drawable.gradient_btn))
+                binding.nextStepBtn.setOnClickListener {
+                    if (newPass == checkingPass) {
                         val clientInfo = SignUpDTO(
                             signUpViewModel.userEmail,
                             newPass,
@@ -52,12 +52,14 @@ class MakingPassFragment : Fragment() {
                         retrofitWork.signUp(clientInfo, successAction = {
                             navController.navigate(R.id.action_makingPassFragment_to_lastSinUpFragment)
                         })
+
+                    }else{
+                        Toast.makeText(context,"비밀번호가 일치하지 않습니다",Toast.LENGTH_SHORT)
                     }
                 }
             }
-        }else{
-
         }
+
 
         binding.nextStepBtn.setOnClickListener {
             navController.popBackStack()
