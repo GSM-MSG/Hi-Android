1020bb6c3b7669f87e007d6f53d0df326de2a20e
GSM-MinSeok <ms0818000@gmail.com>
2023-03-05 17:33:03 +0900

    공지사항 퍼블리싱

--- app/src/main/java/com/example/hiprojecttest/view/SplashActivity.kt
+++ app/src/main/java/com/example/hiprojecttest/view/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.hiprojecttest.R
+import com.example.hiprojecttest.view.notice.NoticeActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
        Handler().postDelayed({
-           val intent = Intent(this, MainActivity::class.java)
+           val intent = Intent(this, NoticeActivity::class.java)
            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
            startActivity(intent)
        },3000)
